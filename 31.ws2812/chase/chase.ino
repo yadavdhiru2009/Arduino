e9092caf5b134f10635f806079c627888a0ae043
yadavdhiru2009 <yadavdhiru2009@gmail.com>
2023-07-26 19:39:45 +0530

    WS2812 LED interface With Arduino

---
+++ 31.ws2812/chase/chase.ino
@@ -0,0 +1,56 @@
+    
+/* 
+ *  Author:dharmendra Kumar Yadav
+ 
+ */
+    #include <FastLED.h>
+    #define LED_PIN     5
+    #define NUM_LEDS    2
+    CRGB leds[NUM_LEDS];
+    void setup() {
+      FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+    }
+    void loop() {
+//    leds[0] = CRGB(255,255,255);
+//    leds[1] = CRGB(255,255,255);
+//    leds[2] = CRGB(255,255,255);
+//    leds[3] = CRGB(255,255,255);
+//    leds[4] = CRGB(255,255,255);
+//    leds[5] = CRGB(255,255,255);
+//    leds[6] = CRGB(255,255,255);
+//    leds[7] = CRGB(255,255,255);
+//    leds[8] = CRGB(255,255,255);
+//    leds[9] = CRGB(255,255,255);
+//    leds[10] = CRGB(255,255,255);
+//    leds[11] = CRGB(255,255,255);
+//    leds[12] = CRGB(255,255,255);
+//    leds[13] = CRGB(255,255,255);
+//    leds[14] = CRGB(255,255,255);
+//    leds[15] = CRGB(255,255,255);
+//    leds[16] = CRGB(255,255,255);
+//    leds[17] = CRGB(255,255,255);
+//    leds[18] = CRGB(255,255,255);
+//    leds[19] = CRGB(255,255,255);
+//    leds[20] = CRGB(255,255,255);
+//    leds[21] = CRGB(255,255,255);;
+//    leds[22] = CRGB(255,255,255);  
+//    leds[23] = CRGB(255,255,255);
+//    leds[24] = CRGB(255,255,255);
+    leds[25] = CRGB(255,255,255);
+    leds[26] = CRGB(255,255,255);
+    leds[27] = CRGB(255,255,255);
+    leds[28] = CRGB(255,255,255);
+    leds[30] = CRGB(255,255,255);
+    leds[31] = CRGB(255,255,255);
+    leds[32] = CRGB(255,255,255);
+    leds[33] = CRGB(255,255,255);
+    leds[34] = CRGB(255,255,255);
+    leds[35] = CRGB(255,255,255);
+    leds[36] = CRGB(255,255,255);
+    leds[37] = CRGB(255,255,255);
+    leds[38] = CRGB(255,255,255);
+    leds[39] = CRGB(255,255,255);
+    leds[40] = CRGB(255,255,255);
+    FastLED.show();
+    //delay(1000);
+}

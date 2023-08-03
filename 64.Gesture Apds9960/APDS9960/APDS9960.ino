499acf9b78abd05287418bac84f800d221c43ec4
yadavdhiru2009 <yadavdhiru2009@gmail.com>
2023-08-03 23:30:49 +0530

    Gesture Sensor APDS9960 Interface with Arduino

---
+++ 64.Gesture Apds9960/APDS9960/APDS9960.ino
@@ -0,0 +1,78 @@
+/*
+Author:Dhrmendra Kumar Yadav
+*/
+#include <Wire.h>
+#include <SparkFun_APDS9960.h>
+ 
+#define APDS9960_INT    2 // Needs to be an interrupt pin
+ 
+// Global Variables
+SparkFun_APDS9960 apds = SparkFun_APDS9960();
+int isr_flag = 0;
+ 
+void setup() {
+ 
+  // Initialize Serial port
+  Serial.begin(9600);
+  Serial.println();
+  Serial.println(F("--------------------------------"));
+  Serial.println(F("APDS-9960 - GestureTest"));
+  Serial.println(F("--------------------------------"));
+ 
+  // Initialize interrupt service routine
+  attachInterrupt(0, interruptRoutine, FALLING);
+ 
+  // Initialize APDS-9960 (configure I2C and initial values)
+  if ( apds.init() ) {
+    Serial.println(F("APDS-9960 initialization complete"));
+  } else {
+    Serial.println(F("Something went wrong during APDS-9960 init!"));
+  }
+ 
+  // Start running the APDS-9960 gesture sensor engine
+  if ( apds.enableGestureSensor(true) ) {
+    Serial.println(F("Gesture sensor is now running"));
+  } else {
+    Serial.println(F("Something went wrong during gesture sensor init!"));
+  }
+}
+ 
+void loop() {
+  if( isr_flag == 1 ) {
+    detachInterrupt(0);
+    handleGesture();
+    isr_flag = 0;
+    attachInterrupt(0, interruptRoutine, FALLING);
+  }
+}
+ 
+void interruptRoutine() {
+  isr_flag = 1;
+}
+ 
+void handleGesture() {
+    if ( apds.isGestureAvailable() ) {
+    switch ( apds.readGesture() ) {
+      case DIR_UP:
+        Serial.println("UP");
+        break;
+      case DIR_DOWN:
+        Serial.println("DOWN");
+        break;
+      case DIR_LEFT:
+        Serial.println("LEFT");
+        break;
+      case DIR_RIGHT:
+        Serial.println("RIGHT");
+        break;
+      case DIR_NEAR:
+        Serial.println("NEAR");
+        break;
+      case DIR_FAR:
+        Serial.println("FAR");
+        break;
+      default:
+        Serial.println("NONE");
+    }
+  }
+}

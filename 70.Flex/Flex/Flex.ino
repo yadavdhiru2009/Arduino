3eb48a2aa5a7195804d475b4f7197b5a411c11ff
yadavdhiru2009 <yadavdhiru2009@gmail.com>
2023-08-04 23:11:10 +0530

    Flex Sensor Interface with Arduino

---
+++ 70.Flex/Flex/Flex.ino
@@ -0,0 +1,20 @@
+/*
+Author Dharmendra Kumar Yadav
+*/
+
+const int flexPin = A0; 
+ 
+void setup() 
+{ 
+  Serial.begin(9600);
+} 
+ 
+void loop() 
+{ 
+  int flexValue;
+  flexValue = analogRead(flexPin);
+  Serial.print("sensor: ");
+  Serial.println(flexValue);
+  
+  delay(20);
+} 
